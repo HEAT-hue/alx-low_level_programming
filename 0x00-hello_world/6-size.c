c163cea4a07b02d7708311c934f29cd4cdcf23b2
mightGuy <onyejemeemmanuel65@gmail.com>
2022-06-30 20:13:24 -0700

    Size

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,6 +12,7 @@ int main(void)
 	float flt = 0.0f;
 	long int lng_int = 0;
 	long long int lng_lng_int = 0;
+
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
 	printf("Size of an int: %d byte(s)\n", sizeof(int));
 	printf("Size of a long int: %d byte(s)\n", sizeof(long int));
