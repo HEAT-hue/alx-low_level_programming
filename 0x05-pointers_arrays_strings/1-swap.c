eadba0f2aa39f101479c238b116dfa565255846f
mightGuy <onyejemeemmanuel65@gmail.com>
2022-07-12 05:21:15 -0700

    Swap variables

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,15 @@
+/**
+ * swap_int -  Resets a variable to 98
+ *
+ * @a: Memory address of first var to be swapped
+ * @b: Memory address of second var to be swapped
+ *
+ * Return void
+ */
+void swap_int(int *a, int *b)
+{
+	int c = *a;
+	
+	*a = *b;
+	*b = c;
+}	
