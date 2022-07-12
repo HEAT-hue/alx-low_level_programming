65eab851558b5df5802f58efcf270428ce457f33
mightGuy <onyejemeemmanuel65@gmail.com>
2022-07-12 05:55:58 -0700

    print characters to stdout

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,19 @@
+#include <unistd.h>
+
+/**
+ * _puts - This prints characters to a string
+ *
+ * @str: The mem addr of the chaaracter to be printed to the string
+ *
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str);
+		++str;
+	}
+
+	_putchar('\n');
+}
