f0136567f2421f579e66f33967b60ad181357f75
mightGuy <onyejemeemmanuel65@gmail.com>
2022-07-12 05:16:47 -0700

    reset a variable

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,11 +1,10 @@
 /**
- * reset_to_9 -  Resets a variable to 98
+ * reset_to_98 -  Resets a variable to 98
  *
  * @n: Memory address of the variable to be reset
  *
  * Return void
  */
-
 void reset_to_98(int *n)
 {
 	*n = 98;
